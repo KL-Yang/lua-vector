5b53c35d57f288ebf2021c3077b84372b105a219
yangkunlun <yangkunlun@gmail.com>
2017-12-06 22:07:06 +0800

    vector_v4 can pass both unit test

--- unit01.lua
+++ unit01.lua
@@ -1,16 +1,17 @@
-print("Start of Unit01")
+print("Start of Unit01 Test")
 
-print("  1.a - create vectors and print")
+print("  1.a - create vectors and print out!")
 a = v.vector({1,2,3,4,5,6,7,8,9})
 b = v.vector(9)         -- 9 zeros
 c = v.matrix(9)         -- 9 zeros
 
 for i=1, 9 do
+    b[i] = i*0.1
     c[i] = 10-i
 end
 
 for i=1, 9 do
-    print("    "..a[i].."  "..b[i].."  "..c[i])
+    print(string.format("%8.2f  %8.2f  %8.2f", a[i], b[i], c[i]))
 end
 
 print("  1.b - vector maths +-*/^")
@@ -18,7 +19,7 @@ e = a+c
 f = a*c+a
 g = c^2+1+a-0.1
 for i=1, 9 do
-    print("    "..e[i].."  "..f[i].."  "..g[i])
+    print(string.format("%8.2f  %8.2f  %8.2f", e[i], f[i], g[i]))
 end
 
 print("End of Unit01")
--- unit02.lua
+++ unit02.lua
@@ -1,20 +1,23 @@
-a = v.matrix(3,9)
+print("Start of Unit01 Test")
 
+print("  2.a - create array of vectors!")
+a = v.matrix(3,9)
 for i=1, 9 do
     a[1][i] = i
-    a[2][i] = 10-i
-    a[3][i] = i*0.1
+    a[2][i] = i*0.1
+    a[3][i] = 10-i 
 end
 
-b = a[1]*2+100
-
-for i=1, 3 do
-    for j=1, 9 do
-        print(a[i][j])
-    end
-end
-
-print("  2.b - vector test")
 for i=1, 9 do
-    print(b[i])
+    print(string.format("%8.2f  %8.2f  %8.2f", a[1][i], a[1][i], a[1][i]))
 end
+
+print("  2.b - vector maths +-*/^")
+e = a[1]+a[3]
+f = a[1]*a[3]+a[1]
+g = a[3]^2+1+a[1]-0.1
+for i=1, 9 do
+    print(string.format("%8.2f  %8.2f  %8.2f", e[i], f[i], g[i]))
+end
+
+print("End of Unit02")
