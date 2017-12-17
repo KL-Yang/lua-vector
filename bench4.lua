3255c689d4059f88e787e950566db4213064aa8b
yangkunlun <yangkunlun@gmail.com>
2017-12-17 23:24:22 +0800

    update 4th matrix benchmark

--- bench4.lua
+++ bench4.lua
@@ -9,9 +9,10 @@ else
     print("LuaJit: "..jit.version)
 end
 
+n = 128
 --2. Initiate test vector
-a = v.matrix(5, 5)
-b = v.matrix(5, 5)
+a = v.matrix(n, n)
+b = v.matrix(n, n)
 for i=1, #a do
     for j=1, #a[i] do
         a[i][j] = 1.0
@@ -22,38 +23,36 @@ end
 --3. Start to operate on RAW!
 ra = v.raw(a)
 rb = v.raw(b)
-print("ra:", ra)
-print("rb:", rb)
-for i=1, #a do
-    print("ra"..i..":", ra[i])
-    print("rb"..i..":", rb[i])
-end
+--print("ra:", ra)
+--print("rb:", rb)
+--for i=1, #a do
+--    print("ra"..i..":", ra[i])
+--    print("rb"..i..":", rb[i])
+--end
 --os.exit(0)
 
-function printmat(x, m, n)
-    for i=1, m do
-        io.write("  ")
-        for j=1, n do
-            io.write(string.format("%9.2f ", x[i][j]))
-        end
-        io.write("\n")
-    end
-end
+--function printmat(x, m, n)
+--    for i=1, m do
+--        io.write("  ")
+--        for j=1, n do
+--            io.write(string.format("%9.2f ", x[i][j]))
+--        end
+--        io.write("\n")
+--    end
+--end
 
-for i=1, 3 do
-    print("Loop "..i)
-    printmat(a, #a, #a[1])
-    for j=1, 5 do
-     --   print("ra:"..ra[j])
-     --   print("rb:"..rb[j])
-        for k=1, 5 do
-            rb[j][k] = rb[j][k]+a[j][k]^3+a[j][k]^2+a[j][k]+1
-            --rb[j][k] = 100.0
+for i=1, 10000 do
+    --print("Loop "..i)
+    --printmat(a, #a, #a[1])
+    for j=1, #a do
+        for k=1, #a[j] do
+            rb[j][k] = rb[j][k]+ra[j][k]^3+ra[j][k]^2+ra[j][k]+1
         end
     end
-    printmat(ra, #a, #a[1])
+    --printmat(ra, #a, #a[1])
 end
 
 --4. RAW rb is just alias of b
-print('b[1][1]='..rb[1][1]..'  b[5][5]='..rb[5][5])
+print('a[1][1]='..ra[1][1]..'  a[n][n]='..ra[n][n])
+print('b[1][1]='..rb[1][1]..'  b[n][n]='..rb[n][n])
 print('finished!')
